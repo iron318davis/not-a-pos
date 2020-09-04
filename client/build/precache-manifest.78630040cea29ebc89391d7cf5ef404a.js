ede56507159d1da679bca59ecc39995b423fd066
Davis Sjoberg <59490187+iron318davis@users.noreply.github.com>
2020-09-03 19:40:28 -0500

    more connection stuff

---
+++ client/build/precache-manifest.78630040cea29ebc89391d7cf5ef404a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d3b1e91d49892aed396bfa2dddcefe3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6470c89b672deb3da40",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "9c8afb8688554168bfe9",
+    "url": "/static/css/main.178cef97.chunk.css"
+  },
+  {
+    "revision": "a6470c89b672deb3da40",
+    "url": "/static/js/2.0bd16f8f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0bd16f8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c8afb8688554168bfe9",
+    "url": "/static/js/main.c26f5f69.chunk.js"
+  },
+  {
+    "revision": "ca9750410536902b8d8b",
+    "url": "/static/js/runtime-main.6e56a85d.js"
+  }
+]);
