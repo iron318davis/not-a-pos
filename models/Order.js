c603156ffd99157e41083f544f9fb4b843233874
JTTJ <JTTJ@Thomass-MacBook-Pro.local>
2020-09-10 21:13:40 -0500

    updated models, associations may need refining

--- models/Order.js
+++ models/Order.js
@@ -1,13 +1,44 @@
 'use strict';
 
-module.exports = function(sequelize, DataTypes) {
-    const Order = sequelize.define("Order", {
-        orderID: DataTypes.INTEGER,
-        employeeID: DataTypes.INTEGER,
-        subtotal: DataTypes.DECIMAL,
-        total: DataTypes.DECIMAL
-    });
-  
+module.exports = function (sequelize, DataTypes) {
+    const Order = sequelize.define(
+        "Order",
+        {
+            orderID: {
+                Type: DataTypes.INTEGER,
+                autoIncrement: true,
+                allowNull: false,
+                primaryKey: true
+            },
+            employeeID: {
+                Type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'Employees',
+                    key: 'employee_id'
+                }
+            },
+            orderDtTm: {
+                Type: DataTypes.DATETIME,
+                defaultValue: Sequelize.NOW
+            },
+            subtotal: {
+                Type: DataTypes.DECIMAL(13,2),
+                allowNull: false
+            },
+            total: {
+                Type: DataTypes.DECIMAL,
+                allowNull: false
+            }
+        },
+        {
+            freezeTableName: true,
+        }
+    );
+    Order.associate = function (models) {
+        Order.belongsTo(models.Employees,
+            { foreignKey: 'employee_id', as: 'employeeID' })
+    };
+
     return Order;
-  };
-  
+};
