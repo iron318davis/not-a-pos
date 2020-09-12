4696b0793145dabd5fb2edb15ff4b70a7255276c
Davis Sjoberg <59490187+iron318davis@users.noreply.github.com>
2020-09-12 12:20:54 -0500

    fixed seed data for latest models/db format

--- db/notapos_menu_seed.sql
+++ db/notapos_menu_seed.sql
@@ -1,43 +1,43 @@
-INSERT INTO `pos_db`.`categories`
-(`category_name`)
-VALUES
- ('Appetizers')
-,('Mains')
-,('Dessert')
-,('Drinks')
-;
-
 INSERT INTO `pos_db`.`menu`
 (`itemName`,
  `categoryID`,
  `price`,
- `item_description`)
+ `item_description`,
+ `createdAt`,
+ `updatedAt`
+)
 VALUES
- ('Fries',1,4.99,'Hand cut, crispy, and delicious')
-,('Onion Rings',1,5.99,'Slim rings in a crispy seasoned shell') 
-,('Cheese Curds',1,6.99,'Squeaky, gooey, and oh-so-good') 
-,('Sampler Platter',1,7.99,'All three of our can''t miss appetizers')  
-,('Hamburger',2,9.99,'An American Classic, unadorned')
-,('Cheeseburger',2,10.99,'An American Classic with American cheese')
-,('Hamburger Deluxe',2,11.99,'An American Classic with extra toppings')
-,('Cheeseburger Deluxe',2,12.99,'An Cheesy Classic with everything on it')
-,('Sundae',3,7.99,'Ice cream and a choice of topping')
-,('Banana Split',3,7.99,'A classic dessert, made healthy with fruit!')
-,('Chocolate Parfait ',3,7.99,'Nothing healthy about hot fudge, peanuts, and ice cream')
-,('Triple Cone',3,7.99,'Can''t spell HUGE without hug -- what it feels like to eat this much ice cream')
-,('Pop',4,2.99,'Pop, Soda, Soda-Pop -- whatever you call it, it''s got bubbles')
-,('Beer',4,4.99,'Nectar of the gods')
-,('Wine',4,4.99,'Not from a box, but this isn''t fancy')
-,('Juice',4,0.99,'Hand squeezed from the bottles we got from the factory')
+ ('Fries',1,4.99,'Hand cut, crispy, and delicious', now(), now())
+,('Onion Rings',1,5.99,'Slim rings in a crispy seasoned shell', now(), now()) 
+,('Cheese Curds',1,6.99,'Squeaky, gooey, and oh-so-good', now(), now()) 
+,('Sampler Platter',1,7.99,'All three of our can''t miss appetizers', now(), now())  
+,('Hamburger',2,9.99,'An American Classic, unadorned', now(), now())
+,('Cheeseburger',2,10.99,'An American Classic with American cheese', now(), now())
+,('Hamburger Deluxe',2,11.99,'An American Classic with extra toppings', now(), now())
+,('Cheeseburger Deluxe',2,12.99,'An Cheesy Classic with everything on it', now(), now())
+,('Sundae',3,7.99,'Ice cream and a choice of topping', now(), now())
+,('Banana Split',3,7.99,'A classic dessert, made healthy with fruit!', now(), now())
+,('Chocolate Parfait ',3,7.99,'Nothing healthy about hot fudge, peanuts, and ice cream', now(), now())
+,('Triple Cone',3,7.99,'Can''t spell HUGE without hug -- what it feels like to eat this much ice cream', now(), now())
+,('Pop',4,2.99,'Pop, Soda, Soda-Pop -- whatever you call it, it''s got bubbles', now(), now())
+,('Beer',4,4.99,'Nectar of the gods', now(), now())
+,('Wine',4,4.99,'Not from a box, but this isn''t fancy', now(), now())
+,('Juice',4,0.99,'Hand squeezed from the bottles we got from the factory', now(), now())
 ;
 
-insert into pos_db.employee_role
+INSERT INTO `pos_db`.`categories`
 (
-`employee_role_id`,
-`roleName`
+`category_name`,
+`createdAt`,
+`updatedAt`
 )
-values
-(1,'FoH');
+VALUES
+ ('Appetizers', now(), now())
+,('Mains', now(), now())
+,('Dessert', now(), now())
+,('Drinks', now(), now())
+,('Sides', now(), now())
+;
 
 Insert into pos_db.employees
 (
@@ -49,27 +49,45 @@ Insert into pos_db.employees
   `address`,
   `city`,
   `phone`,
-  `roleID`
+  `roleID`,
+	`createdAt`,
+	`updatedAt`
 )
 Values
-(1,'first1','last1',1,true,'123 Lane1','Minneapolis1','111-222-3333',1),
-(2,'first2','last2',2,true,'123 Lane2','Minneapolis2','111-222-3333',1);
+(1,'first1','last1',1,true,'123 Lane1','Minneapolis1','111-222-3333',1, now(), now()),
+(2,'first2','last2',2,true,'123 Lane2','Minneapolis2','111-222-3333',1, now(), now());
+
+insert into pos_db.employee_role
+(
+`employee_role_id`,
+`roleName`,
+`createdAt`,
+`updatedAt`
+)
+values
+(1,'FoH', now(), now());
+
+insert into pos_db.OrderItems
+(
+    `orderID`,
+    `menuID`,
+    `itemName`,
+	`createdAt`,
+	`updatedAt`
+)
+values
+(1,1,'TestStaticItemName', now(), now());
+
+-- DO NOT RUN ORDER SCRIPT IF YOU ARE ATTEMPTING TO ACTUALLY MAKE AN ORDER 
 
 insert into pos_db.`order`
 (
     `orderID`,
     `employeeID`,
     `subtotal`,
-    `total`
+    `total`,
+	`createdAt`,
+	`updatedAt`
 )
 values
-(1,1,1,1);
-
-insert into pos_db.OrderItems
-(
-    `orderID`,
-    `menuID`,
-    `itemName`
-)
-values
-(1,1,'TestStaticItemName');
+(1,1,1,1, now(), now());
